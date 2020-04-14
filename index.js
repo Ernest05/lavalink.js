78c06f6f296500271462ab6face6c95c8b88cb83
Ernest05 <kylian.prevot@outlook.com>
2020-04-14 22:35:25 +0200

    Updated index.js

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 'use strict';
 
-if (require('discord.js') && require('eris')) {
-    throw new Error('You can\'t have Discord.js and Eris installed. Please choose one library only.');
-}
-
 module.exports = {
     LavalinkWebsocket: require('./src/LavalinkWebsocket'),
     PlayerInstance: require('./src/PlayerInstance'),
